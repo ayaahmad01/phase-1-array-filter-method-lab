a81f7da0a80756b7b6a078f05fbb9b2f0ad6c330
Aya Abu AlHummos <ayaahmad113@gmail.com>
2023-10-26 14:17:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(array1, something) {
+return array1.filter(
+    (result) => result.toLowerCase() === something.toLowerCase()
+)
+}
+function fuzzyMatch(array2, letter) {
+    return array2.filter(
+    (results) => results.toLowerCase().indexOf(letter.toLowerCase()) === 0
+)
+}
+function matchName(array3, driverName) {
+    return array3.filter((result3) => result3.name === driverName);
+  }
